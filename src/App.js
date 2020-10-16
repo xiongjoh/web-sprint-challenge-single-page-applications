067f8ac4eb78bbe92dc852dd10d78bff160fa55b
unknown <xiongjoh@gmail.com>
2020-10-16 10:17:24 -0500

    install axios,yup,react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <header>
+        <h1>Lambda Eats</h1>
+      </header>
     </>
   );
 };
