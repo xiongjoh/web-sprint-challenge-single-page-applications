94b839df2c7756b6620fd19f7e33b55f251a263f
unknown <xiongjoh@gmail.com>
2020-10-16 11:49:35 -0500

    add functionality to form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link, Route } from "react-router-dom";
 import Home from './components/Home'
 import Form from './components/Form'
-import Confirmation from './components/Confirmation'
 import './App.css'
 
+const initialFormOrder = {
+  name: '',
+  size: '',
+  instructions: '',
+  pepperoni: false,
+  sausage: false,
+  chicken: false,
+  cheese: false,
+}
+
+const initialReceivedOrders = []
+
 const App = () => {
+
+  const [formValues, setFormValues] = useState(initialFormOrder)
+  const [pizzaOrders, setPizzaOrders] = useState(initialReceivedOrders)
+
+  const change = (name, value) => {
+    setFormValues({...formValues, [name]:value})
+  }
+
+  const submit = () => {
+
+  }
+
   return (
     <>
       <header className="App-header">
@@ -16,7 +39,9 @@ const App = () => {
         </nav>
       </header>
       <Route exact path='/' component={Home}/>
-      <Route path='/pizza' component={Form}/>
+      <Route path='/pizza' render={(props) => (
+      <Form {...props} values={formValues} change={change} submit={submit}/>
+      )}/>
     </>
   );
 };
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,44 +2,89 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 export default function Form(props) {
+    const {values, change, submit} = props
+
+    const onSubmit = (evt) => {
+        evt.preventDefault()
+    }
+    const onChange = (evt) => {
+        const {name, value, type, checked} = evt.target
+        const valueToUse = type === "checkbox" ? checked : value
+        change(name, valueToUse)
+    }
+
     return (
-        <form className="pizza-form">
+        <form className="pizza-form" onSubmit={onSubmit}>
             <div className="pizza-list">
             <label>Name: 
-                <input/>
+                <input
+                type='text'
+                name='name'
+                onChange={onChange}
+                value={values.name}
+                />
             </label>
             <label>Pizza Size:
-                <select>
-                    <option>--Select Pizza Size--</option>
-                    <option>Small 10"</option>
-                    <option>Medium 12"</option>
-                    <option>Large 14"</option>
-                    <option>Ex-Large 18"</option>
+                <select
+                onChange={onChange}
+                value={values.role}
+                name='size'
+                >
+                    <option value=''>--Select Pizza Size--</option>
+                    <option value='small'>Small 10"</option>
+                    <option value='medium'>Medium 12"</option>
+                    <option value='large'>Large 14"</option>
+                    <option value='ex-large'>Ex-Large 18"</option>
                 </select>
             </label>
 
             <label>Special Instructions:
-                <input type="text"/>
+                <input 
+                type="text"
+                name="instructions"
+                onChange={onChange}
+                value={values.instructions}
+                />
             </label>
 
             </div>
             <div className="pizza-list">
             <p>Toppings:</p>
             <label>Pepperoni
-                <input type="checkbox"/>
+                <input 
+                type="checkbox"
+                name="pepperoni"
+                checked={values.pepperoni}
+                onChange={onChange}
+                />
             </label>
             <label>Sausage
-                <input type="checkbox"/>
+                <input 
+                type="checkbox"
+                name="sausage"
+                checked={values.sausage}
+                onChange={onChange}
+                />
             </label>
             <label>Chicken
-                <input type="checkbox"/>
+                <input 
+                type="checkbox"
+                name="chicken"
+                checked={values.chicken}
+                onChange={onChange}
+                />
             </label>
             <label>Cheese
-                <input type="checkbox"/>
+                <input 
+                type="checkbox"
+                name="cheese"
+                checked={values.cheese}
+                onChange={onChange}
+                />
             </label>
             </div>
-            
 
+            <button>Order</button>
         </form>
     )
 }
